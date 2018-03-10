d0e45399813a52d21e924f3b8dfc0fe8608c9db2
Omer Dogan <omerdogan3@gmail.com>
2018-03-10 20:38:19 +0300

    Login Logout Google

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,7 @@
 module.exports = {
   googleClientID: '690555011246-9n2n845fg5abbsvmgbe43kh06erbbbvp.apps.googleusercontent.com',
   googleClientSecret: 'RHjMGJCYeW9d-DWL1qV-Pq6_',
-  mongoURI: 'mongodb://kukuli123:12312312@ds249718.mlab.com:49718/react-node-course'
+  mongoURI: 'mongodb://kukuli123:12312312@ds249718.mlab.com:49718/react-node-course',
+  cookieKey: 'asdasfasdgsdrsdadasdasdasdqwpo'
 };
   
