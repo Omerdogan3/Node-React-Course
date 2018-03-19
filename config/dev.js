38b6f7da5703152eab2f3d230026582c2e96fa18
Omer Dogan <omerdogan3@gmail.com>
2018-03-19 16:45:34 +0300

    React Proxy

--- config/dev.js
+++ config/dev.js
@@ -1,6 +1,6 @@
 module.exports = {
-    googleClientID: '690555011246-9n2n845fg5abbsvmgbe43kh06erbbbvp.apps.googleusercontent.com',
-    googleClientSecret: 'RHjMGJCYeW9d-DWL1qV-Pq6_',
+    googleClientID: '981895327043-hgd5f72dna27bnash0cl55p59vqv9b96.apps.googleusercontent.com',
+    googleClientSecret: 'dMSOotfWtQTJLMfE2OdU3EGb',
     mongoURI: 'mongodb://kukuli123:12312312@ds249718.mlab.com:49718/react-node-course',
     cookieKey: 'asdasfasdgsdrsdadasdasdasdqwpo'
   };
