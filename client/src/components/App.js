c613e3208b96af0e3e806eeb6dd4497d43cab11c
Omer Dogan <omerdogan3@gmail.com>
2018-03-24 22:02:02 +0300

    Materialize Css - Header Design

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import {BrowserRouter, Route} from 'react-router-dom'; 
 
-const Header = () => <h2>Header</h2>;
+import Header from './Header';
+
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
 const Landing = () => <h2>Landing</h2>;
@@ -12,8 +13,10 @@ const App = () => {
         <div>
             <BrowserRouter>
                 <div>
+                    <Header/>
                     <Route exact={true} path="/" component={Landing} />
-                    <Route path="/surveys" component={Dashboard}/>
+                    <Route exact path="/surveys" component={Dashboard}/>
+                    <Route path="/surveys/new" component={SurveyNew}/>
                 </div>
             </BrowserRouter>
         </div>
