514aa00bbc79fb89950d761b762bf186f00adfe0
Omer Dogan <omerdogan3@gmail.com>
2018-03-26 17:34:42 +0300

    Redux Setup - axios,redux-thunk

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {BrowserRouter, Route} from 'react-router-dom'; 
 
 import Header from './Header';
@@ -8,19 +8,21 @@ const SurveyNew = () => <h2>SurveyNew</h2>;
 const Landing = () => <h2>Landing</h2>;
 
 
-const App = () => {
-    return(
-        <div>
-            <BrowserRouter>
-                <div>
-                    <Header/>
-                    <Route exact={true} path="/" component={Landing} />
-                    <Route exact path="/surveys" component={Dashboard}/>
-                    <Route path="/surveys/new" component={SurveyNew}/>
-                </div>
-            </BrowserRouter>
-        </div>
-    );
+class App extends Component {
+    render(){
+        return(
+            <div className="container">
+                <BrowserRouter>
+                    <div>
+                        <Header/>
+                        <Route exact={true} path="/" component={Landing} />
+                        <Route exact path="/surveys" component={Dashboard}/>
+                        <Route path="/surveys/new" component={SurveyNew}/>
+                    </div>
+                </BrowserRouter>
+            </div>
+        );
+    }
 };
 
 export default App;
