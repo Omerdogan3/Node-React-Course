0dd4011cde69b4595c2d0658365c6d6cd40008c7
Omer Dogan <omerdogan3@gmail.com>
2018-03-28 22:49:49 +0300

    Fetch User

--- client/src/index.js
+++ client/src/index.js
@@ -10,7 +10,7 @@ import App from './components/App';
 import reducers from './reducers';
 
 
-const store = createStore((reducers)=>[], {}, applyMiddleware(reduxThunk));
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 
 ReactDOM.render(
